72ede51910be17ff40fdf96f993449ba7f2896c7
SaracenOne <SaracenOne@gmail.com>
2022-09-01 02:54:29 +0100

    Bump version number

--- ml_amt/Properties/AssemblyInfo.cs
+++ ml_amt/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("AvatarMotionTweaker (Saracen Tweaks)")]
-[assembly: AssemblyVersion("1.0.3.1")]
-[assembly: AssemblyFileVersion("1.0.3.1")]
+[assembly: AssemblyVersion("1.0.3.2")]
+[assembly: AssemblyFileVersion("1.0.3.2")]
 
-[assembly: MelonLoader.MelonInfo(typeof(ml_amt.AvatarMotionTweaker), "AvatarMotionTweaker (Saracen Tweaks)", "1.0.3.1", "SDraw/Saracen", "https://github.com/SaracenOne/ml_mods_cvr")]
+[assembly: MelonLoader.MelonInfo(typeof(ml_amt.AvatarMotionTweaker), "AvatarMotionTweaker (Saracen Tweaks)", "1.0.3.2", "SDraw/Saracen", "https://github.com/SaracenOne/ml_mods_cvr")]
 [assembly: MelonLoader.MelonGame(null, "ChilloutVR")]
 [assembly: MelonLoader.MelonPlatform(MelonLoader.MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
 [assembly: MelonLoader.MelonPlatformDomain(MelonLoader.MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
